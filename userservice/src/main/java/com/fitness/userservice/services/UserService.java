42b6ecd0a93b99daec0e3e45c208fdce06466b85
Sushmita-Hubli <hublisushmita7@gmail.com>
2026-08-01 18:35:05 -0400

    created basic endpoints for user
    register user
    get user

---
+++ userservice/src/main/java/com/fitness/userservice/services/UserService.java
@@ -0,0 +1,57 @@
+package com.fitness.userservice.services;
+
+import com.fitness.userservice.dto.RegisterRequest;
+import com.fitness.userservice.dto.UserResponse;
+import com.fitness.userservice.models.User;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Service
+@AllArgsConstructor
+public class UserService {
+
+    private final UserRepository repository;
+
+    public UserResponse register(RegisterRequest request) {
+        if(repository.existsByEmail(request.getEmail()))
+        {
+            throw new RuntimeException("Email already exists");
+
+        }
+
+        User user = new User();
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+        user.setEmail(request.getEmail());
+        user.setPassword(request.getPassword());
+
+        User savedUser = repository.save(user);
+        UserResponse userResponse = new UserResponse();
+        userResponse.setId(savedUser.getId());
+        userResponse.setFirstName(savedUser.getFirstName());
+        userResponse.setLastName(savedUser.getLastName());
+        userResponse.setEmail(savedUser.getEmail());
+        userResponse.setPassword(savedUser.getPassword());
+        userResponse.setCreatedAt(savedUser.getCreatedAt());
+        userResponse.setUpdatedAt(savedUser.getUpdatedAt());
+
+        return userResponse;
+    }
+
+    public UserResponse getUserProfile(String userId) {
+        User user=repository.findById(userId).orElseThrow(()-> new RuntimeException("User not found"));
+        UserResponse userResponse=new UserResponse();
+        userResponse.setFirstName(user.getFirstName());
+        userResponse.setLastName(user.getLastName());
+        userResponse.setEmail(user.getEmail());
+        userResponse.setPassword(user.getPassword());
+        userResponse.setCreatedAt(user.getCreatedAt());
+        userResponse.setUpdatedAt(user.getUpdatedAt());
+        userResponse.setId(user.getId());
+        return userResponse;
+    }
+}

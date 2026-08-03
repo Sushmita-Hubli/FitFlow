172ff420a20dd81e82b068efb05c30e8d0fe13d0
Sushmita-Hubli <hublisushmita7@gmail.com>
2026-08-03 15:53:50 -0400

    ai model integration and recommendation generation

--- aiservice/src/main/java/com/fitness/aiservice/AiserviceApplication.java
+++ aiservice/src/main/java/com/fitness/aiservice/AiserviceApplication.java
@@ -7,7 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AiserviceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AiserviceApplication.class, args);
+
+        System.out.println("GEMINI_URL = " + System.getenv("GEMINI_URL"));
+        System.out.println("GEMINI_KEY = " + System.getenv("GEMINI_KEY"));
+        SpringApplication.run(AiserviceApplication.class, args);
 	}
+
 
 }
